1f86568b28bf0f2fd328f9120d310c27cae55f4f
Lezzycan <waheedolalekan23@example.com>
2023-03-23 04:00:02 +0100

    modified main_page

--- lib/pages/nav_pages/main_page.dart
+++ lib/pages/nav_pages/main_page.dart
@@ -1,6 +1,6 @@
 import 'package:bloc_travel_app/pages/nav_pages/account_page.dart';
 import 'package:bloc_travel_app/pages/nav_pages/bar_page.dart';
-import 'package:bloc_travel_app/pages/nav_pages/home_page.dart';
+import 'package:bloc_travel_app/pages/home_page.dart';
 import 'package:bloc_travel_app/pages/nav_pages/search_page.dart';
 import 'package:flutter/material.dart';
 
